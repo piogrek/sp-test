88e9b19a72f6d48df8b6c3da93732e3e217bff82
Piotr Grochowski <piogrek@gmail.com>
2016-11-03 12:00:41 +0000

    enabling prod mode

--- symfony/web/app.php
+++ symfony/web/app.php
@@ -7,7 +7,7 @@ use Symfony\Component\Debug\Debug;
 $loader = require __DIR__.'/../app/autoload.php';
 //Debug::enable();
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
