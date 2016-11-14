1763401c77e5689b72f52926321133fa12d2fc3a
Piotr Grochowski <piogrek@gmail.com>
2016-11-14 10:47:07 +0000

    updating docker config

--- php-fpm/start.sh
+++ php-fpm/start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-cd /var/www/symfony
+#cd /var/www/symfony
 composer install -n
-bin/console doctrine:schema:update --force
-bin/console cache:clear --env=prod
-php-fpm -F
+bin/console doctrine:schema:update --force -n
+#bin/console cache:clear --env=prod -n
+echo "-----------------------------------------------"
+echo "-----------------------------------------------"
+echo "-----------------------------------------------"
+echo "http://localhost:8081"
+echo "-----------------------------------------------"
+echo "-----------------------------------------------"
+echo "-----------------------------------------------"
+bin/console server:run 0.0.0.0:8081 -n -q
+
